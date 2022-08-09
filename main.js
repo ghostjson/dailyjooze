f46ba145a34087ef95a53a0925d4b555248574e5
lemokami <jesswinw@gmail.com>
2022-08-10 01:09:52 +0530

    feat: add homepage

--- main.js
+++ main.js
@@ -1,47 +1,52 @@
-const express = require("express");
-const fs = require("fs");
+const express = require('express');
+const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const adminCredentials = {
-  username: process.env.DJ_USERNAME || "admin",
-  password: process.env.DJ_PASSWORD || "17291234",
+  username: process.env.DJ_USERNAME || 'admin',
+  password: process.env.DJ_PASSWORD || '17291234',
 };
 
 app.use(express.json());
+app.use(express.static('./public'));
 
-app.post("/api/v1/download", (req, res) => {
+app.get('/', (req, res) => {
+  res.render('./index.html');
+});
+
+app.post('/api/v1/download', (req, res) => {
   const downloads = req.body.downloads;
   if (req.body.email || req.body.whatsapp) {
     console.log(req.body.email, req.body.whatsapp);
 
     fs.writeFileSync(
-      __dirname + "/downloads/users.csv",
+      __dirname + '/downloads/users.csv',
       `\n${req.body.email},${req.body.whatsapp}`,
-      { flag: "a+" }
+      { flag: 'a+' }
     );
 
     if (downloads.daily && downloads.weekly && downloads.tracker) {
-      res.sendFile(__dirname + "/downloads/all.zip");
+      res.sendFile(__dirname + '/downloads/all.zip');
     } else if (downloads.daily && downloads.weekly) {
-      res.sendFile(__dirname + "/downloads/DJdaily-DJweekly.zip");
+      res.sendFile(__dirname + '/downloads/DJdaily-DJweekly.zip');
     } else if (downloads.daily && downloads.tracker) {
-      res.sendFile(__dirname + "/downloads/DJdaily-DJtarget.zip");
+      res.sendFile(__dirname + '/downloads/DJdaily-DJtarget.zip');
     } else if (downloads.daily) {
-      res.sendFile(__dirname + "/downloads/DJdaily.pdf");
+      res.sendFile(__dirname + '/downloads/DJdaily.pdf');
     } else if (downloads.tracker) {
-      res.sendFile(__dirname + "/downloads/DJtarget.pdf");
+      res.sendFile(__dirname + '/downloads/DJtarget.pdf');
     } else if (downloads.weekly) {
-      res.sendFile(__dirname + "/downloads/DJweekly.pdf");
+      res.sendFile(__dirname + '/downloads/DJweekly.pdf');
     } else {
-      res.status(204).send("No file selected");
+      res.status(204).send('No file selected');
     }
   } else {
-    res.status(401).send("Should provide either email or whatsapp");
+    res.status(401).send('Should provide either email or whatsapp');
   }
 });
 
-app.post("/api/v1/getData", (req, res) => {
+app.post('/api/v1/getData', (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
 
@@ -52,15 +57,15 @@ app.post("/api/v1/getData", (req, res) => {
       username === adminCredentials.username &&
       password === adminCredentials.password
     ) {
-      res.sendFile(__dirname + "/downloads/users.csv");
+      res.sendFile(__dirname + '/downloads/users.csv');
     } else {
-      res.status(401).send("Username or password incorrect");
+      res.status(401).send('Username or password incorrect');
     }
   } else {
-    res.status(401).send("Username or password incorrect");
+    res.status(401).send('Username or password incorrect');
   }
 });
 
 app.listen(PORT, () => {
-  console.log("Listening to the port " + PORT);
+  console.log('Listening to the port ' + PORT);
 });
